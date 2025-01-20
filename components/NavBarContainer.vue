d9d4996a6eeed9d0d877a2e769089e568c183ca9
joelaz <joelzorri@gmail.com>
2025-01-20 09:36:18 -0300

    account register and company register

--- components/NavBarContainer.vue
+++ components/NavBarContainer.vue
@@ -9,22 +9,22 @@ const items = [
 	{
 		text: 'Listar Clientes',
 		icon: 'mdi-text-account',
-		link: '/client-list',
+		link: 'client/client-list',
 	},
 	{
 		text: 'Nuevo Cliente',
 		icon: 'mdi-account-plus-outline',
-		link: '/new-client',
+		link: 'client/new-client',
 	},
 	{
 		text: 'Productos',
 		icon: 'mdi-clipboard-list-outline',
-		link: '/product-list',
+		link: 'product/product-list',
 	},
 	{
 		text: 'Nuevo Productos',
 		icon: 'mdi-tag-plus',
-		link: '/new-procuct',
+		link: 'product/new-procuct',
 	},
 
 	{ text: 'Settings', icon: 'mdi-cog', notifications: 8, link: '/subastas' },
---
+++ pages/auth/login.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const showDialog = ref(true)
+const type = ref<String>('password')
+const fields = ref({
+	email: '',
+	password: '',
+})
+</script>
+
+<template>
+	<div class="sc-flex sc-w-full">
+		<div class="sc-w-8/12 sc-p-5">
+			<v-card class="!sc-rounded-3xl sc-mt-5 sc-p-5 sc-w-full">
+				<div class="!sc-px-10 sc-mx-10 sc-my-2">
+					<div class="sc-flex sc-items-end sc-justify-end"></div>
+					<v-form class="sc-p-5">
+						<label class="sc-text-black">Correo electrónico</label>
+						<v-text-field
+							v-model="fields.email"
+							variant="solo"
+							color="primary"
+							prepend-inner-icon="mdi-email-outline"
+							class="sc-mt-2"
+							type="email"
+							placeholder="Correo electrónico"
+						/>
+						<label class="sc-text-black">Contraseña</label>
+						<v-text-field
+							v-model="fields.password"
+							:type="type"
+							variant="solo"
+							color="primary"
+							prepend-inner-icon="mdi-lock-outline"
+							class="sc-mt-2"
+							placeholder="Contraseña"
+							append-inner-icon="mdi-eye-outline"
+						/>
+						<v-btn type="button" block class="sc-my-5 !sc-rounded-lg !sc-py-6" color="primary"> Iniciar Sesión </v-btn>
+					</v-form>
+
+					<div class="sc-flex sc-flex-col sc-items-center sc-py-3">
+						<p class="sc-text-black sc-text-center sc-mt-5 sc-leading-7">¿Has olvidado tu contraseña? <br /></p>
+						<div class="sc-text-primary sc-font-bold sc-cursor-pointer">Recuperar contraseña</div>
+						<p class="sc-text-black sc-text-center sc-mt-5 sc-leading-7">
+							¿Aún no te has registrado?
+							<br />
+						</p>
+						<div @click="" class="sc-text-primary sc-font-bold sc-cursor-pointer">Crea tu cuenta aquí</div>
+					</div>
+				</div>
+			</v-card>
+		</div>
+	</div>
+</template>
